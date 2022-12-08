4eec5961545610dc9f735bebc97cf5065c7eb3fd
Daniel Nichols <dando18studios@gmail.com>
2022-12-08 10:07:56 -0500

    added timer

--- 2022/include/utilities.hpp
+++ 2022/include/utilities.hpp
@@ -6,6 +6,7 @@
  * 
  */
 // stl includes
+#include <chrono>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -43,6 +44,36 @@ bool hasDuplicates(Iterator first, Iterator last) {
     std::unordered_set<std::iter_value_t<Iterator>> set(first, last);
     return set.size() != static_cast<size_t>( std::distance(first, last) );
 }
+
+class Timer {
+public:
+    void start() noexcept {
+        start_ = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop() noexcept {
+        end_ = std::chrono::high_resolution_clock::now();
+    }
+
+    int64_t getDurationSeconds() const noexcept {
+        return std::chrono::duration_cast<std::chrono::seconds>(end_ - start_).count();
+    }
+
+    int64_t getDurationMilliseconds() const noexcept {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(end_ - start_).count();
+    }
+
+    int64_t getDurationMicroseconds() const noexcept {
+        return std::chrono::duration_cast<std::chrono::microseconds>(end_ - start_).count();
+    }
+
+    int64_t getDurationNanoseconds() const noexcept {
+        return std::chrono::duration_cast<std::chrono::nanoseconds>(end_ - start_).count();
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_{}, end_{};
+};
 
 
 } // end namespace util
